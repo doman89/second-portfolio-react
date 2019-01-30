5258672f1fae3a834918769bab2bb64a01fafb2f
doman89 <mateusz.domanski.1989@gmail.com>
2019-01-30 21:16:49 +0100

    Built Navigation
    Is written the navigation of portfolio website. Added some styles and
    handle events methods. Navigation is responsive - has 4 break points.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './header/Header';
 
 class App extends Component {
+
+  state={
+    headerBottomOffset: 0,
+    activeSection: 'welcome!',
+  };
+  headerOffset = null;
+
+  handleChangeSection = event => {
+    console.log(this);
+    this.setState({
+      activeSection: event.target.textContent,
+    });
+  };
+
+  handleScroll = event => {
+    // console.log(window.pageXOffset);
+    console.log(window.scrollY);
+    // if(window.querySelector('header').pageYOffset / 2
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <>
+      <Header
+          handleChangeSection={this.handleChangeSection}
+          onScroll={this.handleOnScroll}
+      />
+          <nav>Here is the end of this site</nav>
+          <section>{this.state.activeSection}</section>
+        </>
     );
   }
+
+  componentDidMount() {
+          window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+          window.removeEventListener('scroll', this.handleScroll);
+  }
+
 }
 
 export default App;
