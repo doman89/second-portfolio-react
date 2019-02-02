017a2ee64aef3b7f910d8cd141cb8968a58caa5d
doman89 <mateusz.domanski.1989@gmail.com>
2019-02-02 21:05:54 +0100

    Some styles changed for testing on iPhone

--- src/content/Welcome.js
+++ src/content/Welcome.js
@@ -5,9 +5,11 @@ const Welcome = () => {
     return (
         <main >
             <article className={'app-content__section-welcome'}>
-                <h1 className={'app-content__section-welcome__title'}>Hi! I am Mateusz Domanski
-                    <p>Are you looking for junior developer?</p>
-                    <p>I would like to invite you, to see my portfolio.</p>
+                <h1 className={'app-content__section-welcome__title'}>
+                    <p className={'app-content__section-welcome__title__main'}>Hi! I am</p>
+                    <p className={'app-content__section-welcome__title__main'}>Mateusz Domanski</p>
+                    <p className={'app-content__section-welcome__title__subtitle'}>Are you looking for junior developer?</p>
+                    <p className={'app-content__section-welcome__title__subtitle'}> I would like to invite you, to see my portfolio.</p>
                 </h1>
                 <div className={'app-content__section-welcome__photo'}>
                     <img src={myPhotography} alt={'My photography'}/>
