1cef8ca87da12994eaa4b087471c2be840ab4ddd
doman89 <mateusz.domanski.1989@gmail.com>
2019-02-02 21:24:55 +0100

    Some styles changed for testing on iPhone

---
+++ src/subnavigation/SubNavigationButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Projects = props => {
+    return (
+        <button className={'app-nav__list__item__btn'}>{props.textContent}
+        </button>
+    );
+};
+
+export default Projects;
